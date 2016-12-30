636a8251135ed28bff7ab4cf02979c5f2167aed3
lilin <lin.li@fgecctv.com>
2016-12-30 13:47:37 +0800

    # 编写界面

--- CodeSource/FamilyName/app/src/main/java/com/lilin/familyname/ui/MainActivity.java
+++ CodeSource/FamilyName/app/src/main/java/com/lilin/familyname/ui/MainActivity.java
@@ -1,22 +1,26 @@
 package com.lilin.familyname.ui;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.lilin.familyname.R;
 import com.lilin.familyname.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
+    @BindView(R.id.tv_father)
+    TextView tvFather;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
 
     }
-
-
 
 }
